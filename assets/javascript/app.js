4cea02da060e0871db0062cc3fc8c628cceee2fb
mswalls <mswalls@rocketmail.com>
2018-08-12 13:52:39 -0400

    html layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA2xqRTLo6X5Ct1YdlTbdVtrodg8seEdMc",
+    authDomain: "train-timers-843b5.firebaseapp.com",
+    databaseURL: "https://train-timers-843b5.firebaseio.com",
+    projectId: "train-timers-843b5",
+    storageBucket: "train-timers-843b5.appspot.com",
+    messagingSenderId: "364856094709"
+  };
+  firebase.initializeApp(config);
